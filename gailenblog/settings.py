0f27991139923b82242dee1f1573b06e65c3d2f2
iamgillespie <gailen.gillespie@gmail.com>
2022-06-15 16:53:03 -0500

    security changes

--- gailenblog/settings.py
+++ gailenblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import random, string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%nf^gxs_i!t-0yeaurbm@7t3x$g#-r6i_=&*q3&2=(%*zd8c*l'
+# Randomized security key
+size = 100
+SECRET_KEY = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.punctuation + string.hexdigits + string.digits, k = size))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 #'*'
